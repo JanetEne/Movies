5d6962f8efe7c719aa90911ba8a916f01b5ec30d
JanetEne <janeteneogenyi@gmail.com>
2020-07-23 10:34:49 +0100

    fix lint errors

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,4 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
-});
-
 module.exports = router;
