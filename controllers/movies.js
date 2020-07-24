a33487a540f03dba6520de4e02153a8c45cafccd
JanetEne <janeteneogenyi@gmail.com>
2020-07-24 22:20:21 +0100

    get all movies from db endpoint

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,16 @@
+import models from '../models/index'
+
+const moviesModel = models.Movies
+
 class Movies {
   static welcome(req, res) {
     res.status(200).send({ message: 'Welcome to Movies Api' })
+  }
+
+  static getAllMovies(req, res) {
+    moviesModel.findAll().then((movies) => {
+      res.status(200).send({ message: 'Movies fetched successfully', movies })
+    })
   }
 }
 
--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,9 @@
 import express from 'express'
 
+import Movies from '../controllers/movies'
+
 const router = express.Router();
+
+router.get('/movies', Movies.getAllMovies)
 
 module.exports = router;
