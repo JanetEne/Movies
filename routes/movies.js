9f0c6a38a22ec5a267ee61f2636538692cc16190
JanetEne <janeteneogenyi@gmail.com>
2020-08-01 18:55:18 +0100

    validate user signUp

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import validateAddMovie from '../middleware/movies'
+import validateAddMovie from '../middleware/validatemovies'
 import Movies from '../controllers/movies'
 
 const router = express.Router();
